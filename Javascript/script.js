1c7c13145a3a45a914dfe37fac5b5afb47caba99
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-10-29 10:36:11 +0100

    feat:?: added

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,6 +4,16 @@ let TotaleKm = prompt("Inserisci quanti Km devi percorrere");
 
 let EtaPasseggero = prompt("Inserisci quanti anni hai");
 
+TotaleKm = TotaleKm.replace(",", ".");
+
+const TotaleKmNum = isNaN(TotaleKm)
+  ? alert("Inserisci km correttamente ")
+  : parseFloat(TotaleKm);
+
+const EtaPasseggeroNum = isNaN(EtaPasseggero)
+  ? alert("Inserisci età correttamente ")
+  : parseInt(EtaPasseggero);
+
 // Variabili globali
 const costoPerKm = 0.21;
 
@@ -13,40 +23,22 @@ let percSconto = 0;
 
 let TotaleSconto = 0;
 
-// Sostituisco la virgola con un punto per poter convertire la string in float
-TotaleKm = TotaleKm.replace(",", ".");
+// Se stai fra i 12 ed i 18 anni mettiamo uno sconto del 20%
+if (EtaPasseggero < 18 && EtaPasseggero > 12) percSconto = 20;
+// Se stai fra i 65 ed i 100 anni mettiamo uno sconto del 40%
+else if (EtaPasseggero > 65 && EtaPasseggero < 100) percSconto = 40;
 
-// Controlli
-if (isNaN(TotaleKm)) alert("Inserisci i km correttamente");
+// Otteniamo il totale sconto in euro
+TotaleSconto = ((TotaleKm * costoPerKm) / 100) * percSconto;
 
-if (isNaN(EtaPasseggero)) alert("Inserisci la tua età correttamente");
+// Totale costo - Totale sconto
+prezzoTotale = TotaleKm * costoPerKm - TotaleSconto;
 
-// Se sia il TotaleKm che l'EtaPasseggero sono valori numerici validi
-if (!isNaN(TotaleKm) && !isNaN(EtaPasseggero)) {
-  // Convertiamo in numero i km
-  TotaleKm = parseFloat(TotaleKm);
-
-  // Convertiamo in numero l'eta del passeggero
-  EtaPasseggero = parseInt(EtaPasseggero);
-
-  // Se stai fra i 12 ed i 18 anni mettiamo uno sconto del 20%
-  if (EtaPasseggero < 18 && EtaPasseggero > 12) percSconto = 20;
-  // Se stai fra i 65 ed i 100 anni mettiamo uno sconto del 40%
-  else if (EtaPasseggero > 65 && EtaPasseggero < 100) percSconto = 40;
-
-  // Otteniamo il totale sconto in euro
-  TotaleSconto = ((TotaleKm * costoPerKm) / 100) * percSconto;
-
-  // Totale costo - Totale sconto
-  prezzoTotale = TotaleKm * costoPerKm - TotaleSconto;
-
-  // Se hai 12 anni o meno torna a casa e l'if si interrompe
-  if (EtaPasseggero <= 12) {
-    alert("Torna a casa bimbo");
-  }
-  // Se hai più di 100 anni ti sei perso? e l'if si interrompe
-  else if (EtaPasseggero >= 100) {
-    alert("Si è perso");
-  } else
-    console.log(`Il prezzo del biglietto è di € ${prezzoTotale.toFixed(2)}`);
+// Se hai 12 anni o meno torna a casa e l'if si interrompe
+if (EtaPasseggero <= 12) {
+  alert("Torna a casa bimbo");
 }
+// Se hai più di 100 anni ti sei perso? e l'if si interrompe
+else if (EtaPasseggero >= 100) {
+  alert("Si è perso");
+} else console.log(`Il prezzo del biglietto è di € ${prezzoTotale.toFixed(2)}`);
